12f97f218af27845e0c3c18be621d1e4af8f3d0c
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-05-06 14:49:49 -0300

    ajuste

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -11,9 +11,10 @@ function Inicio() {
         height="315"
         src="https://www.youtube.com/embed/O-YzJEd3xQ4?si=p9fEoGTQvzKDUalG"
         title="YouTube video player"
-        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
       <Link to="/questoes" className="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition-all">
         Ir para questões
       </Link>
