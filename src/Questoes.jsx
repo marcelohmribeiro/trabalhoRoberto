945f0dc533987d56b0e3027bcf66061fe01f0b41
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-05-06 11:35:14 -0300

    .

--- src/Questoes.jsx
+++ src/Questoes.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { questions } from "./Questoes.js"
+import { questions } from "./questionsData"
 import { Link } from "react-router-dom"
 
 function Questoes() {
